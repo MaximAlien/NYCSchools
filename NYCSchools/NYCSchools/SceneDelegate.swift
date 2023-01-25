12da0000dd394e2d4b4a99836a141b8c3bc4d2fc
Maxim Makhun <maxim.makhun@gmail.com>
2023-01-24 19:07:00 -0800

    Add NYCSchools project skeleton.

---
+++ NYCSchools/NYCSchools/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  NYCSchools
+//
+//  Created by Maxim Makhun on 24.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+}
---
+++ NYCSchools/NYCSchools/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by Maxim Makhun on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
